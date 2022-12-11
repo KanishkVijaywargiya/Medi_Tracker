00bded4b1ece68dcefc14c86270b0f66ac05c432
KanishkVijaywargiya <secretcodersociety@gmail.com>
2022-12-11 15:30:16 +0530

    Initial Commit

---
+++ MediTracker/MediTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediTrackerApp.swift
+//  MediTracker
+//
+//  Created by Kanishk Vijaywargiya on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MediTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
